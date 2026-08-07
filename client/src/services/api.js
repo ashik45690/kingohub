6d806f9b65a3b67a8c95f29d3de0b343711a77db
ashik45690 <ashikedavakkath@gmail.com>
2026-08-07 11:24:34 +0530

    Fix production URLs

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,46 +1,21 @@
-import axios from 'axios';
-
-<!-- const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
-    withCredentials: true
-});
-
-// Add a request interceptor to attach token
-api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
-    }
-);
-
-export default api; -->
-
-
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://kingohub.onrender.com/api',
-    withCredentials: true
+  baseURL: "https://kingohub.onrender.com/api",
+  withCredentials: true,
 });
 
-// Add a request interceptor to attach token
 api.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem('token');
-        if (token) {
-            config.headers.Authorization = `Bearer ${token}`;
-        }
-        return config;
-    },
-    (error) => {
-        return Promise.reject(error);
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
 );
 
 export default api;
